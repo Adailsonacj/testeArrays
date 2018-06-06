4f2a32e75827bb2e5a8474f99ba05b45bd8ec813
Adailsonacj <adailsonacj@live.com>
2018-06-05 21:58:47 -0300

    05/06/2018 21:58

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String...args){
+        int[] poltronas = new int[100];
+
+        for(int i=0; i>100; i++){
+            poltronas[i] = 1;
+        }
+        System.out.println(poltronas.length);
+    }
+}
